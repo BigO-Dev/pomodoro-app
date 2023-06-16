ecbf9aab023f9ab85d35988474175ca1d86a28bd
Omair Majid <omajid.dev@gmail.com>
2023-06-16 23:07:20 +0000

    Add function to allow the styles of the buttons to change when clicked

--- script.js
+++ script.js
@@ -21,8 +21,14 @@ function countdown(minutes) {
       isBreak = !isBreak;
       console.log(`Countdown ${isBreak ? "complete!" : "started!"}`);
       if (isBreak) {
-        // If break ended, start work countdown
-        countdown(25);
+        countdown(5); // Short break by default
+        shortBreakButton.classList.add("btn-active");
+        longBreakButton.classList.remove("btn-active");
+      } else {
+        countdown(25); // Work session by default
+        startButton.classList.add("btn-active");
+        shortBreakButton.classList.remove("btn-active");
+        longBreakButton.classList.remove("btn-active");
       }
     }
   }, 1000);
@@ -32,24 +38,45 @@ function setShortBreak() {
   clearInterval(timer);
   isBreak = true;
   countdown(5);
+  shortBreakButton.classList.add("btn-active");
+  longBreakButton.classList.remove("btn-active");
 }
 
 function setLongBreak() {
   clearInterval(timer);
   isBreak = true;
   countdown(10);
+  shortBreakButton.classList.remove("btn-active");
+  longBreakButton.classList.add("btn-active");
 }
 
 startButton.addEventListener("click", () => {
   clearInterval(timer);
   isBreak = false;
   countdown(25);
+  startButton.classList.add("btn-active");
+  startButton.classList.remove("btn-inactive");
+  shortBreakButton.classList.remove("btn-active");
+  longBreakButton.classList.remove("btn-active");
+  shortBreakButton.classList.add("btn-inactive");
+  longBreakButton.classList.add("btn-inactive");
 });
 
 shortBreakButton.addEventListener("click", () => {
   setShortBreak();
+  startButton.classList.add("btn-inactive");
+  shortBreakButton.classList.remove("btn-inactive");
+  longBreakButton.classList.remove("btn-active");
+  shortBreakButton.classList.add("btn-active");
+  longBreakButton.classList.add("btn-inactive");
 });
 
 longBreakButton.addEventListener("click", () => {
   setLongBreak();
+  startButton.classList.add("btn-inactive");
+  startButton.classList.remove("btn-active");
+  shortBreakButton.classList.remove("btn-active");
+  longBreakButton.classList.remove("btn-inactive");
+  shortBreakButton.classList.add("btn-inactive");
+  longBreakButton.classList.add("btn-active");
 });
